60d10b8deba39fd67d11739d23d51c75d436bcb1
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-09-21 11:51:53 +0530

    add task list

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,7 +4,7 @@ import "./Task.css"
 const Task = ({id, title, description, priority}) => {
     return (
         <div className='task-container'>
-            <h1 className='task-title'>{title}</h1in>
+            <h1 className='task-title'>{title}</h1>
             <p className='task-description'>{description}</p>
             <span className='task-priority'> 🎯{priority}</span>
             </div>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,51 +4,100 @@ import Task from "./../../components/Task/Task"
 import "./Home.css"
 
 export const Home = () => {
-    const [taskList, setTaskList] =useState([
+    const [taskList, setTaskList] = useState([
         {
             id: 1,
             title: 'Submit assignment',
             description: 'Nahi to gali padegi',
             priority: 'high'
-                 
+
         },
-        {
-            id: 2,
-            title: 'Go To Market',
-            description: 'Buy Alu & Gobi',
-            priority: 'medium'
-                 
-        },
-        {
-            id: 3,
-            title: 'Go To Market',
-            description: 'Buy Alu & Gobi',
-            priority: 'medium'
-                 
-        },
+        // {
+        //     id: 2,
+        //     title: 'Go To Market',
+        //     description: 'Buy Alu & Gobi',
+        //     priority: 'medium'
+
+        // },
+       
     ])
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [priority, setpriority] = useState('')
+
+
+    const addTaskToList = () => {
+        const randomId = Math.floor(Math.random() * 1000);
+        const obj = {
+            id: 'randomId',
+            title: title,
+            description: description,
+            priority: priority
+        }
+        setTaskList([...taskList, obj])
+
+        setTitle('');
+        setDescription('');
+        setpriority('');
+    }
+
     return (
         <div className='container'>
             <h1 className='app-title'> Daily Task Minder 📃</h1>
-           
-           <div className='todo-flex-container'>
-             <div>
-                <h2 className='text-center'>Show List</h2>
-                {
-                    taskList.map((taskItem, index)=>{
-                        const{id, title, description, priority} =taskItem;
-                        return <Task id={id} title={title} description={description} priority={priority}
-                        />
-                    })
-                }
-             </div>
 
-             <div>
-                <h2 className='text-center'>Add List</h2>
-             </div>
+            <div className='todo-flex-container'>
+                <div>
+                    <h2 className='text-center'>Show List</h2>
+                    {
+                        taskList.map((taskItem, index) => {
+                            const { id, title, description, priority } = taskItem;
+                            return <Task id={id} title={title} description={description} priority={priority}
+                            />
+                        })
+                    }
+                </div>
 
-           </div>
+                <div>
+                    <h2 className='text-center'>Add List</h2>
+                    <div className='add-task-form-container'>
 
+                        <form>
+                            <input type='text'
+                                value={title}
+                                onChange={(e) => {
+                                    setTitle(e.target.value)
+                                }}
+                                placeholder='Enter Title'
+                                className='task-input' />
+
+
+                            <input
+                                type='text'
+                                value={description}
+                                onChange={(e) => {
+                                    setDescription(e.target.value)
+                                }}
+                                placeholder='Enter Description'
+                                className='task-input' />
+
+                            <input
+                                type='text'
+                                value={priority}
+                                onChange={(e) => {
+                                    setpriority(e.target.value)
+                                }}
+                                placeholder='Enter Priority'
+                                className='task-input' />
+
+                            <button className='btn-add-task' type='button' onClick={addTaskToList}>
+                                Add Task to List
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
